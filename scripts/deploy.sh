c258a34d852f6f1d131442e255b54190db11f5ca
Rob Young <rob@robyoung.digital>
2019-10-25 08:56:31 +0100

    Only deploy if the binary has changed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+TARGET_DIR="./target/arm-unknown-linux-gnueabihf/release"
+TARGET="${TARGET_DIR}/glow"
+
 fail() {
   >&2 echo $1
   exit 1
@@ -12,10 +15,16 @@ fail() {
 [ -n "$IFTTT_WEBHOOK_KEY" ] || fail "IFTT webhook key is required"
 
 
-ssh $BOBNET_GLOW_DEPLOY_TARGET hostname
-rustup run nightly cargo build --release --target=arm-unknown-linux-gnueabihf
-scp ./target/arm-unknown-linux-gnueabihf/release/glow $BOBNET_GLOW_DEPLOY_TARGET:
+ssh $BOBNET_GLOW_DEPLOY_TARGET hostname > /dev/null
+rustup run nightly cargo build --quiet --release --target=arm-unknown-linux-gnueabihf
+md5sum --quiet -c ${TARGET}.md5sum && {
+  >&2 echo "No change to release binary"
+  exit 1
+}
+ssh $BOBNET_GLOW_DEPLOY_TARGET sudo systemctl stop glow.service
+scp ${TARGET} $BOBNET_GLOW_DEPLOY_TARGET:
 
 cat glow.service | envsubst | ssh $BOBNET_GLOW_DEPLOY_TARGET "cat >/tmp/glow.service"
 ssh $BOBNET_GLOW_DEPLOY_TARGET "cmp /tmp/glow.service /etc/systemd/system/glow.service || { sudo mv /tmp/glow.service /etc/systemd/system/glow.service && sudo systemctl daemon-reload ; }"
-
+ssh $BOBNET_GLOW_DEPLOY_TARGET sudo systemctl start glow.service
+md5sum ${TARGET} > ${TARGET}.md5sum
